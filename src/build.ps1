b73425acf1877fe0f2fc3e70a34c0fd8806d2967
Bastian Eicher <bastian@eicher.net>
2020-08-12 15:52:05 +0200

    Improved CI pipeline

--- src/build.ps1
+++ src/build.ps1
@@ -2,15 +2,23 @@
 $ErrorActionPreference = "Stop"
 pushd $PSScriptRoot
 
-if (Test-Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe") {
-  $vsDir = . "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -products * -property installationPath -format value -version 16.5
-  $msBuild = if (Test-Path "$vsDir\MSBuild\Current") {"$vsDir\MSBuild\Current\Bin\amd64\MSBuild.exe"} else {"$vsDir\MSBuild\15.0\Bin\amd64\MSBuild.exe"}
-} else {
-  throw "Needs Visual Studio 2019 v16.5 or newer"
+function Find-MSBuild {
+    if (Test-Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe") {
+        $vsDir = . "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -products * -property installationPath -format value -version 16.5
+        if ($vsDir) {
+            if (Test-Path "$vsDir\MSBuild\Current") { return "$vsDir\MSBuild\Current\Bin\amd64\MSBuild.exe" } else { return "$vsDir\MSBuild\15.0\Bin\amd64\MSBuild.exe" }
+        }
+    }
 }
 
-if ($env:CI) { $ci = "-p:ContinuousIntegrationBuild=True" }
-. $msBuild -v:Quiet -t:Restore -t:Build $ci -p:Configuration=Release -p:Version=$Version
-if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
+function Run-MSBuild {
+    $msbuild = Find-Msbuild
+    if (!$msbuild) { throw "You need Visual Studio 2019 v16.5+ to build this project" }
+    . $msbuild @args
+    if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
+}
+
+if ($env:CI) { $ci = "/p:ContinuousIntegrationBuild=True" }
+Run-MSBuild /v:Quiet /t:Restore /t:Build $ci /p:Configuration=Release /p:Version=$Version
 
 popd
