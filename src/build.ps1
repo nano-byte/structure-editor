dc0693b2f93b057dc4a8d4e92dc3cc04cde34eea
Bastian Eicher <bastian@eicher.net>
2023-12-27 21:16:58 +0100

    Added .NET 8.0 build target

--- src/build.ps1
+++ src/build.ps1
@@ -12,7 +12,7 @@ function Find-MSBuild {
 }
 
 function Run-DotNet {
-    ..\0install.ps1 run --batch --version 6.0.. https://apps.0install.net/dotnet/sdk.xml @args
+    ..\0install.ps1 run --batch --version 8.0.. https://apps.0install.net/dotnet/sdk.xml @args
     if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 }
 
