bba0673704a1feae7fa2044a6976bf2dc01d6228
Bastian Eicher <bastian@eicher.net>
2026-08-21 00:44:14 +0200

    Aligned IStructureEditor type constraint with StructureEditor

--- src/StructureEditor/IStructureEditor.cs
+++ src/StructureEditor/IStructureEditor.cs
@@ -1,6 +1,7 @@
 // Copyright Bastian Eicher
 // Licensed under the MIT License
 
+using System;
 using System.Diagnostics.CodeAnalysis;
 
 namespace NanoByte.StructureEditor;
@@ -10,7 +11,7 @@ namespace NanoByte.StructureEditor;
 /// </summary>
 /// <typeparam name="T">The type of object to edit.</typeparam>
 public interface IStructureEditor<T>
-    where T : class
+    where T : class, IEquatable<T>
 {
     /// <summary>
     /// Adds a <see cref="ContainerDescription{TContainer}"/> used to describe the structure of the data being edited.
