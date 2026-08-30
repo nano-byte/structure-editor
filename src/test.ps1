9216a94a5e180c63fc92eb856cd41a5026ea322f
Bastian Eicher <bastian@eicher.net>
2026-08-30 19:46:52 +0200

    Tweaked CI script

--- src/test.ps1
+++ src/test.ps1
@@ -6,6 +6,6 @@ function Run-DotNet {
     if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 }
 
-Run-DotNet test --no-build --configuration Release UnitTests\UnitTests.csproj
+Run-DotNet test --no-build --configuration Release
 
 popd
